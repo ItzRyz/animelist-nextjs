46e5d106e1936e46cf19afaaeefce7b4400d1378
kisa22 <irvanagamingyt@gmail.com>
2024-04-23 14:56:50 +0700

    -

--- src/app/api/v1/collection/route.js
+++ src/app/api/v1/collection/route.js
@@ -7,15 +7,16 @@ export async function POST(req) {
     const data = await req.json();
     if (!data.user_email && !data.anime_mal_id)
       return NextResponse.json({ message: "Bad Request on Collection" }, { status: 400 });
-    const createCollection = await prisma.collection.create({ data }); // baca docs bang, baca docs nya dari google aja jgn ke webnya nextjs
+    const createCollection = await prisma.collection.create({ data });
     if (createCollection) {
-      // bahasa di docs nextjs terlalu tinggi
-      return NextResponse.json({ message: "Collection Successfully Added" }, { status: 200 }); // kalo anime_mal_id nya unique gk bakal bisa
+      return NextResponse.json({ message: "Collection Successfully Added" }, { status: 200 });
     } else return NextResponse.json({ message: "Bad Request on Collection" }, { status: 400 });
   } catch (e) {
     return NextResponse.json(
       { message: "Internal server Error", error: e.message },
-      { status: 500 } // bikin sign in tanpa nextauth, coba bikin auth api sendiri, sehari selesai ini mah pake jwt & bcrypt (encrypt password) jgn lupa biar secure
-    ); // bebas itu mah, mau di taro node_modules jg bisa klo ngerti cara bikin package
+      { status: 500 }
+    );
   }
-} // oiya client side nya aman?
+}
+
+//! pr  // bikin sign in tanpa nextauth, coba bikin auth api sendiri, pake jwt & bcrypt (encrypt password)
